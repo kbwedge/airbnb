1795bc352fc2e2027102c6e825c554a584673340
kbwedge <kevin.wedge@kellogg.northwestern.edu>
2022-01-16 00:31:18 +0000

    Exercise Complete

--- practice-6.sql
+++ practice-6.sql
@@ -15,6 +15,7 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-SELECT neighborhood, COUNT(*)
+SELECT listings.neighborhood, COUNT(reviews.id)
 FROM reviews
-GROUP BY neighborhood;
+    INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY listings.neighborhood;
